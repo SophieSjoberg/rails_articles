29c2a4425015717cee920be492700ca82b1504c5
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-27 17:44:53 +0100

    Add more steps to feature and create Article model

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,6 +2,12 @@ When("I am on the landing page") do
   visit '/'
 end
 
+Given("the following articles exists") do |table|
+  table.hashes.each do |hash|
+    Article.create!(hash)
+  end
+end
+
 Then("I should see {string}") do |content|
   expect(page).to have_content content
 end
---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :article do
+    title "Article title"
+    content "Content of article"
+  end
+end
