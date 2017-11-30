487d03bc38a7f00f6d7d38fb00a892919da73bab
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-30 11:43:32 +0100

    Create feature and step defs for create, add implementations

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -2,17 +2,16 @@ Given("I visit the landing page") do
   visit root_path
 end
 
-When("I click {string} link") do |string|
-  click_link_or_button string
+When("I click {string} link") do |link|
+  click_link_or_button link
 end
 
-
-When("I fill in {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I fill in {string} with {string}") do |field_name, content|
+fill_in field_name, with: content
 end
 
-When("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click {string} button") do |button|
+  click_button button
 end
 
 Then("I should be on {string} page") do |string|
