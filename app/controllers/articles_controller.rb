6055138b8fe7dea2f9b484d13f172244e7cc6cf8
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-03 21:24:55 +0100

    All tests for create pass

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,27 @@
 class ArticlesController < ApplicationController
-  # def new
-  #   @article = Article.new
-  # end
-  #
-  # def create
-  #   article = Article.new
-  #   article.title = params[:article][:title]
-  #   article.content = params[:article][:content]
-  #   article.save
-  #   flash[:notice] = 'Article was successfully created.'
-  #   redirect_to article
-  # end
-  #
-  # def show;
-  # end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+
+    if @article.save
+      flash[:notice] = 'Article was successfully created.'
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params[:article].permit(:title, :content)
+
+  end
 end
