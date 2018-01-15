d3fe7104aa6e1df9393dd483669ca6f7a1eb9bb9
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-01-15 08:51:48 +0100

    Modify feature and step defs to include author and created at

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,7 +2,7 @@ When("I am on the landing page") do
   visit root_path
 end
 
-Given("the following articles exists") do |table|
+Given("the following articles exist") do |table|
   table.hashes.each do |article|
     FactoryBot.create(:article, article)
   end
@@ -10,4 +10,8 @@ end
 
 Then("I should see {string}") do |content|
   expect(page).to have_content content
+end
+
+Then("show me the page") do
+  save_and_open_page
 end
